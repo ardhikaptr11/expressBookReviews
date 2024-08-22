e65044af3a6a237127914be5f5a55ad3c082ba00
Ardhika Putra <ardhikaptr11@gmail.com>
2024-08-22 13:39:49 +0700

    Fixing indentation

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const session = require("express-session");
+
 const customer_routes = require("./router/auth_users.js").authenticated;
 const genl_routes = require("./router/general.js").general;
 
@@ -18,7 +19,7 @@ app.use("/customer/auth/*", function auth(req, res, next) {
 	if (req.session.authorization) {
 		let token = req.session.authorization["accessToken"];
 
-		jwt.verify(token, "access", (user, err) => {
+		jwt.verify(token, "access", (err, user) => {
 			if (!err) {
 				req.user = user;
 				next();
